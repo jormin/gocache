ae802279d3d2fc02d8b529b40f47e73fe54ab4c3
Jormin <jorminxee@gmail.com>
2020-10-06 16:58:43 +0800

    :sparkles: feat(0.1.01): 封装完成 cache 包
    Signed-off-by: Jormin <jorminxee@gmail.com>

---
+++ chapter1/server/cache/cache.go
@@ -0,0 +1,8 @@
+package cache
+
+type Cache interface {
+	Set(string, []byte) error
+	Get(string) ([]byte, error)
+	Delete(string) error
+	GetStat() Stat
+}
---
+++ chapter1/server/cache/new.go
@@ -0,0 +1,16 @@
+package cache
+
+import "log"
+
+// 创建并返回 Cache 接口
+func New(typ string) Cache {
+	var c Cache
+	switch typ {
+	case "inmemory":
+		c = new(inMemoryCache)
+	default:
+		panic("unknown cache type " + typ)
+	}
+	log.Println(typ, "ready to serve")
+	return c
+}
